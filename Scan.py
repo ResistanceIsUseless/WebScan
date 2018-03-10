809ad4deaa6e2da1b22e16e780d41815a1b8ecc6
mgriffiths <matt.griffiths@gmail.com>
2018-03-10 11:32:43 -0800

    finished storing paths, started argparser

--- Scan.py
+++ Scan.py
@@ -1,20 +1,39 @@
 #/usr/bin/python
 #Webscanner
+#TD: functions
+#TD: Error handling for missing pages, passive info gathering, exporting data for other uses, hand off work to other scanners, api fuzzing
 import requests
+import argparse
+import urllib.parse
 
-url = "http://www.epa.gov/robots.txt"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("host", help="URL you wish to scan")
+parser.add_argument("path", help="Scan using specific path")
+parser.add_argument("robots", help="Check for robots.txt and check for disallow's")
+args = parser.parse_args()
+if args.robots:
+#    print("Checking Robots")
+#if args.u:
+
+host = "http://www.epa.gov"
+path = "/robots.txt"
+url = urllib.parse.urljoin(host, path)
 
 headers = {
     'Cache-Control': "no-cache",
     'Postman-Token': "00b85c94-437c-492d-bbaa-cfb0215421ad"
     }
-
+print(url)
 response = requests.request("GET", url, headers=headers)
 r = response.text
+rpaths = []
+for lines in response.text.split("\n"):
+    if "Disallow:" in lines:
+        rpaths.append(lines.partition('Disallow: ')[2])
 
-for line in response.text.split("\n"):
-#This is doing the opposite, looking online i probably need another for loop
-    if "Disallow" in line:
-        print(line)
-    else:
-        print("ignore")
+
+    #else:
+        #print("ignore")
+
+print(rpaths)
