e8b4cd07f58bf1e64e35e4aef0f5bd340266fedb
mgriffiths <matt.griffiths@gmail.com>
2018-03-12 19:40:15 -0700

    arg parse update

--- Scan.py
+++ Scan.py
@@ -15,26 +15,34 @@ args = parser.parse_args()
 if args.robots:
     print("Checking Robots")
     args.path = 'robots.txt'
+    webhost = args.host
+    path = args.path
+    url = urllib.parse.urljoin(webhost, path)
+
+    headers = {
+        'Cache-Control': "no-cache",
+        'Postman-Token': "00b85c94-437c-492d-bbaa-cfb0215421ad"
+    }
+    print(url)
+    response = requests.request("GET", url, headers=headers)
+    rpaths = []
+    for lines in response.text.split("\n"):
+        if "Disallow:" in lines:
+            rpaths.append(lines.partition('Disallow: ')[2])
+
+    print(rpaths)
+
+    for scan in response:
+        url = urllib.parse.urljoin(webhost, path)
+        response = requests.request("GET", url, headers=headers)
+        if rstatus_code == '200':
+            print(url)
+
+#finished robots, now iterate though each path
+
+
+        # else:
+        # print("ignore")
+
 #if args.host:
 
-whost = args.host
-path = args.path
-url = urllib.parse.urljoin(whost, path)
-
-headers = {
-    'Cache-Control': "no-cache",
-    'Postman-Token': "00b85c94-437c-492d-bbaa-cfb0215421ad"
-    }
-print(url)
-response = requests.request("GET", url, headers=headers)
-r = response.text
-rpaths = []
-for lines in response.text.split("\n"):
-    if "Disallow:" in lines:
-        rpaths.append(lines.partition('Disallow: ')[2])
-
-
-    #else:
-        #print("ignore")
-
-print(rpaths)
