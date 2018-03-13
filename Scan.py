c1ddc170bbb09ccae9290d7cd1a1151204c08717
mgriffiths <matt.griffiths@gmail.com>
2018-03-13 16:09:06 -0700

    now checks all stored disallows for valid pages or not

--- Scan.py
+++ Scan.py
@@ -1,10 +1,9 @@
 #/usr/bin/python
-#Webscanner
-#TD: functions
-#TD: Error handling for missing pages, passive info gathering, exporting data for other uses, hand off work to other scanners, api fuzzing
-import requests
-import argparse
-import urllib.parse
+#Webscanner - Pykto
+#TD: Make function for iterating through pages with parameters for all options
+#TD: Decide on default scanning type (dirbuster or nikto)
+#TD: Error handling(connection, missing pages,rtc),exporting data , hand off work to other scanners, api fuzzing,passive info gathering
+import requests, argparse, urllib.parse
 
 #argument parsing and helptext
 parser = argparse.ArgumentParser()
@@ -31,18 +30,15 @@ if args.robots:
             rpaths.append(lines.partition('Disallow: ')[2])
 
     print(rpaths)
-
-    for scan in response:
+#iterate through disallow lines checking status codes
+    for path in rpaths:
         url = urllib.parse.urljoin(webhost, path)
         response = requests.request("GET", url, headers=headers)
-        if rstatus_code == '200':
-            print(url)
+        print(url)
+        print(response.status_code)
+        if response.status_code == '200':
+            print("Success:" + url + response.status_code)
 
-#finished robots, now iterate though each path
-
-
-        # else:
-        # print("ignore")
 
 #if args.host:
 
