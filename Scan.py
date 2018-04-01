b465a98a700e027187e0681cc9363b0f9c3ddf91
mgriffiths <matt.griffiths@gmail.com>
2018-03-31 19:02:21 -0700

    stage two, file searching started

--- Scan.py
+++ Scan.py
@@ -3,7 +3,7 @@
 #TD: Make function for iterating through pages with parameters for all options
 #TD: Decide on default scanning type (dirbuster or nikto)
 #TD: Error handling(connection, missing pages,rtc),exporting data , hand off work to other scanners, api fuzzing,passive info gathering
-import requests, argparse, urllib.parse
+import requests, argparse, urllib.parse,
 
 #argument parsing and helptext
 parser = argparse.ArgumentParser()
@@ -33,12 +33,52 @@ if args.robots:
 #iterate through disallow lines checking status codes
     for path in rpaths:
         url = urllib.parse.urljoin(webhost, path)
-        response = requests.request("GET", url, headers=headers)
-        print(url)
-        print(response.status_code)
-        if response.status_code == '200':
-            print("Success:" + url + response.status_code)
+        response = requests.request("HEAD", url, headers=headers)
+        #print(url)
+        #print(response.status_code)
+        if response.status_code == requests.codes.ok:
+            print("Success:" + str(url) +" STATUS: "+ str(response.status_code))
 
+if args.host
+#scan through top domain checking for vulnerable files
+    print("Base Scan \\n Checking for files")
 
-#if args.host:
+    args.path = 'robots.txt'
+    webhost = args.host
+    path = args.path
+    url = urllib.parse.urljoin(webhost, path)
+
+    headers = {
+        'Cache-Control': "no-cache",
+         'Postman-Token': "00b85c94-437c-492d-bbaa-cfb0215421ad"
+    }
+    response = requests.request("GET", url, headers=headers)
+    for lines in response.text.split("\n"):
+        if "allow:" in lines:
+            rpaths.append(lines.partition('allow: ')[2])
+
+    print(rpaths)
+rpaths_good = []
+found_files = []
+# iterate through robots checking status codes
+# prob should create new object to store valid paths after printing them
+    for path in rpaths:
+        url = urllib.parse.urljoin(webhost, path)
+        response = requests.request("HEAD", url, headers=headers)
+        # print(url)
+        # print(response.status_code)
+        if response.status_code == requests.codes.ok:
+            print("Success:" + str(url) + " STATUS: " + str(response.status_code))
+            rpaths_good.append(path)
+ #import file and try against found paths as well as root directory
+    open(files.txt, str, fpath)
+    for path in rpaths_good
+        url = urllib.parse.urljoin(webhost, path)
+        response = requests.request("HEAD", url, headers=headers)
+        # print(url)
+        # print(response.status_code)
+        if response.status_code == requests.codes.ok:
+        print("Success:" + str(url) + " STATUS: " + str(response.status_code))
+        found_files.append(path)
+
 
